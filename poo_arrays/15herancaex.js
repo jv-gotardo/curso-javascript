e52ae9b3ad6c934fa30b47320301801a15ebea16
Joao Vitor <jvitorgotardo0514@gmail.com>
2024-06-17 23:47:04 -0300

    Adiciona um eventListener e um map ao exercício de herança

--- poo_arrays/15herancaex.js
+++ poo_arrays/15herancaex.js
@@ -1,11 +1,51 @@
 let ftiponormal = document.querySelector("#ftiponormal");
 let ftipoa302 = document.querySelector("#ftipoa302");
 let fquilom = document.querySelector("#fquilom");
+let fnome = document.querySelector("#fnome");
+let fpais = document.querySelector("#fpais");
+let fveiculo = document.querySelector("#fveiculo");
+let marcas = document.querySelector("#marcas");
+let btnaddmarca = document.querySelector("#btnaddmarca");
+
+let asmarcas = [];
+
+let limparCampos=()=>{
+    fnome.value="";
+    fpais.value="";
+    fveiculo.value="";
+    fquilom.value=0;
+}
 
 ftipoa302.addEventListener("click", ()=>{
+    limparCampos();
     fquilom.removeAttribute("disabled");
 })
-ftipo
+ftiponormal.addEventListener("click", ()=>{
+    limparCampos();
+    fquilom.setAttribute("disabled", "disabled");
+})
+
+let adicionarMarcas=()=>{
+    marcas.innerHTML = "";
+    asmarcas.map((c)=>{
+        let div = document.createElement("div");
+        div.setAttribute("class", "marca");
+        div.innerHTML = (`Nome: ${c.nome} <br/>País: ${c.pais} <br/>Veículo:${c.veiculo}`);
+        marcas.appendChild(div);
+    })
+}
+
+btnaddmarca.addEventListener("click", ()=>{
+    if(ftiponormal.checked){
+        asmarcas.push(new Marca(fnome.value, fpais.value, fveiculo.value));
+    }else{
+        //Não é possível usar polimorfismo pois um dos parâmetros não existe na classe-pai.
+        asmarcas.push(new Marca(fnome.value, fpais.value, fveiculo.value,
+            fquilom.value));
+    }
+    adicionarMarcas();
+})
+
 
 class Marca{ 
     constructor(nome, pais, veiculo){
